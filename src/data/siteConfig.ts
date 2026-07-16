6ff1178c862c89f4dc710bd7d2ac9f39f1a120d9
nandahaese <nandahaese@gmail.com>
2026-07-16 20:45:50 -0300

    Add real social links

--- src/data/siteConfig.ts
+++ src/data/siteConfig.ts
@@ -2,9 +2,9 @@ export const siteConfig = {
   name: "Fernanda Haese",
   email: "hello@fernandahaese.com",
   socialLinks: {
-    behance: "https://www.behance.net/",
-    linkedin: "https://www.linkedin.com/",
-    github: "https://github.com/",
+    behance: "https://www.behance.net/nandahaese",
+    linkedin: "https://www.linkedin.com/in/fernanda-haese/",
+    github: "https://github.com/FernandaHaese",
   },
   cvPath: "/files/fernanda-haese-cv.pdf",
 } as const;
